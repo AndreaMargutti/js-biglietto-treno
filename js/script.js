9bab6319a18e90904edf2e3884d087a4f4865cf6
AndreaMargutti <marguttiandrea30@gmail.com>
2024-07-31 14:50:33 +0200

    creazione variabili età/km

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 /* Scaletta Mentale
-1- 
+1. Chiedere età e Km all'utente
+2. Definire il prezzo base del biglietto
+    2.1. Calcolare lo sconto per i minorenni
+    2.2 Calcolare lo sconto per gli over 65
+3. Definire gli sconti e quando vengono applicati
+4. Stampare l'output in console
 */
 
 // # Fase di preparazione
 
 // # Fase di raccolta dati
+//Richiesta età
+const passengerAge = parseInt(prompt('Quanti anni hai?', '18'));
+console.log('passengerAge:', passengerAge);
+
+//Richiesta km percorsi
+const passengerKm = parseInt(prompt('Quanti km vuoi percorrere?', '20'));
+console.log('passengerKm:', passengerKm);
 
 // # Fase di elaborazione dati
 
